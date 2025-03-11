37cdffbb206185c38f66e9ae3130b027e8296665
leon-bekker <ST10470417@vcconnect.edu.za>
2025-03-11 10:41:14 +0200

    did some changes
    did some changes and added stuff

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -11,7 +11,12 @@ package com.mycompany.prjclasses;
 public class PrjClasses {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! I just did github and hate reece now :) ");
+        
+        newClass nc = new newClass();
+        
+        nc.helloClass(); // have to use variable name nc to communicate with the class
+        
+        System.out.println(nc.strSecondClassVar); // output variable
         
     }
 }
